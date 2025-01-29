3b104a8ae21f5fff5102cdc67fc6e97866d16c78
Gabriel-marques-do-Nascimento <gabrielmarquesnascimento21@gmail.com>
2025-01-29 11:19:03 -0300

    update

--- back/app/Rotas/events.py
+++ back/app/Rotas/events.py
@@ -1,13 +1,28 @@
 from flask import  Blueprint
 from flask_socketio import emit, SocketIO, join_room, leave_room
+from datetime import  datetime
+from Database.usuarios import  Users, Messages, db
 
 socket_bp = Blueprint("socket_pb",__name__)
 
 def socket_register(socketio: SocketIO):
 	
 	@socketio.on("channel")
-	def channel(data):
+	def channel(data: dict):
+		user = Users.query.filter_by(id=data["id"]).first()
+		user.online = datetime.utcnow()
+		d_user = Users.query.filter_by(id=data["d-id"]).first()
+		
+		msg_db= Messages(user=user, message=data["message"], pessoaId=data.get("d-id"), senderId=data.get("id"))
+		
+		dest_msg_db = Messages(user=user,pessoaId=data.get("d-id"), message=data.get("message"), senderId=data.get("id"))
+		db.session.add(dest_msg_db)
+		db.session.add(msg_db)
+		db.session.commit()
+		
 		emit("channel", data, to=data["room"], broadcast=True)
+	
+	
 	
 	@socketio.on("join")
 	def join(data):
--- web/js/to_send.js
+++ web/js/to_send.js
@@ -1,12 +1,26 @@
 import { URL } from "./env.js";
 import { new_msg } from "./script.js";
 
+function room(user_id, from_id){
+	if (parseInt(user_id) <= parseInt(from_id)){
+		
+	}
+	else {
+		return toString(from_id) + toString(user_id)
+	}
+}
+
+
+
+
 const input = document.getElementById("msg-input");
 const send_button = document.getElementById("send");
 let token = localStorage.getItem("1463token-as-savekjg");
 send_button.addEventListener("click", () => {
 	new_msg(input.value)
 	input.value = null
+	let user = localStorage.getItem("hiveid")
+	let from = localStorage.getItem("HiveSender")
     // fetch(`${URL}/send_msg`, {
     //     method: "POST",
     //     headers: {

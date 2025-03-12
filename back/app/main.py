691a16b7b9b252f218cae19df16ce62c34aa2f5b
Gabriel marques do nascimento <gm2042225@gmail.com>
2025-03-12 12:54:05 -0300

    Update main.py

--- back/app/main.py
+++ back/app/main.py
@@ -48,5 +48,5 @@ def mymesgs(token):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT") or 5000)
     socketIo.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
